9b9708a83db6764e88cff058c06cf46730761116
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-02-15 21:59:06 +0700

    Fix typo association in Skor model

--- src/models/skor.js
+++ src/models/skor.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Skor.belongsTo(models.Alternatif, { foreignKey: 'kode_alternatif', as: 'alternatif' });
+      Skor.belongsTo(models.alternatif, { foreignKey: 'kode_alternatif', as: 'alternatif' });
     }
   }
   Skor.init(
