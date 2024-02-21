ad1afcafdf2e28eb0adc4d199011c305f6bfed24
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-02-21 15:04:22 +0700

    feat: crud nilai target

--- src/routes/nilaitarget.js
+++ src/routes/nilaitarget.js
@@ -20,4 +20,70 @@ router.get('/table', async (req, res, next) => {
   return res.json(jsonToTable(data));
 });
 
+router.post('/', async (req, res, next) => {
+  const data = req.body;
+  const user_id = req.session.userId;
+  const findNilaiTarget = await nilai_target.findOne({ where: { kode: data.kode, user_id } });
+  if (findNilaiTarget) {
+    req.flash('error', 'Kode Nilai Target Tidak Boleh Sama');
+    return res.redirect('/nilaitarget');
+  }
+  await nilai_target.create({ user_id, name: data.name, kode: data.kode, value: data.value, category: data.category });
+  req.flash('success', 'Nilai Target Berhasil Ditambahkan');
+  return res.redirect('/nilaitarget');
+});
+
+router.post('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const data = req.body;
+  const user_id = req.session.userId;
+  const findNilaiTarget = await nilai_target.findOne({ where: { kode: data.kode, user_id } });
+  if (findNilaiTarget) {
+    await nilai_target.update({ name: data.name, value: data.value, category: data.category }, { where: { id, user_id } });
+    req.flash('success', 'Nilai Target Berhasil Diubah');
+    return res.redirect('/nilaitarget');
+  } else {
+    await nilai_target.update({ name: data.name, kode: data.kode, value: data.value, category: data.category }, { where: { id, user_id } });
+    req.flash('success', 'Nilai Target Berhasil Diubah');
+    return res.redirect('/nilaitarget');
+  }
+});
+
+router.get('/delete/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const user_id = req.session.userId;
+  // find nilai target by id
+  const findNilaiTarget = await nilai_target.findOne({ where: { id, user_id } });
+  if (findNilaiTarget) {
+    await nilai_target.destroy({ where: { id, user_id } });
+  }
+  req.flash('success', 'Nilai Target Berhasil Dihapus');
+  return res.redirect('/nilaitarget');
+});
+
+router.get('/form', async (req, res, next) => {
+  return res.render('nilaitarget/form', { 
+    title: 'Nilai Target', 
+    action: '/nilaitarget',
+    formName: '',
+    formKode: '',
+    formValue: '',
+    formCategory: '',
+  });
+});
+
+router.get('/form/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const user_id = req.session.userId;
+  const target = await nilai_target.findOne({ where: { id, user_id } });
+  return res.render('nilaitarget/form', { 
+    title: 'Nilai Target', 
+    action: `/nilaitarget/${id}`,
+    formName: target.name,
+    formKode: target.kode,
+    formValue: target.value,
+    formCategory: target.category,
+  });
+});
+
 module.exports = router;
