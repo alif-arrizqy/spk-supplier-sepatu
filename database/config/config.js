e217430df19af39e40295aff6eeae0061e5ddeab
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-02-26 16:00:39 +0700

    Update database config to use different port in production

--- database/config/config.js
+++ database/config/config.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
 
+let PORT;
 const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  PORT = process.env[`${env.toUpperCase()}_DB_PORT`] || 5432;
+} else if (env === 'production') {
+  PORT = process.env[`${env.toUpperCase()}_DB_PORT_EXPOSE`] || 5433;
+} 
 
 const baseConfig = {
   username: process.env[`${env.toUpperCase()}_POSTGRES_USER`],
   password: process.env[`${env.toUpperCase()}_POSTGRES_PASSWORD`],
   database: process.env[`${env.toUpperCase()}_POSTGRES_DB`],
   host: process.env[`${env.toUpperCase()}_DB_HOST`],
-  port: process.env[`${env.toUpperCase()}_DB_PORT`] || 5432,
+  port: PORT,
   dialect: 'postgres',
 };
+
 
 module.exports = {
   development: { ...baseConfig },
